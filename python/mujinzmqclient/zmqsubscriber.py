b918bcbcb56ff415da7700599dcb024cb2bdb600
sergey.pavlov <sergey.pavlov@mujin.co.jp>
2025-02-13 16:11:26 +0900

    [zmqsubscriber] Use threadInterval by copy as it can be modified any moment.

--- python/mujinzmqclient/zmqsubscriber.py
+++ python/mujinzmqclient/zmqsubscriber.py
@@ -310,12 +310,14 @@ class ZmqThreadedSubscriber(ZmqSubscriber):
                 except Exception as e:
                     log.exception('exception caught in subscriber thread "%s": %s', self._threadName, e)
                 # rate limit the loop if desired
-                if self._threadInterval is None:
+                # Make a copy and use it because self._threadInterval is not protected by any mutex and can be modified any moment.
+                threadInterval = self._threadInterval
+                if threadInterval is None:
                     continue
                 elapsedTime = GetMonotonicTime() - starttime
-                if elapsedTime > self._threadInterval:
+                if elapsedTime > threadInterval:
                     continue
-                time.sleep(self._threadInterval - elapsedTime)
+                time.sleep(threadInterval - elapsedTime)
         except Exception as e:
             log.exception('exception caught in subscriber thread "%s": %s', self._threadName, e)
         finally:
